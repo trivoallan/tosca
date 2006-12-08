de00bd25609decbe4eab70a11b501e21dd1d144b
mloiseleur <mloiseleur>
2006-12-08 10:21:46 +0000

    *** empty log message ***

--- cvsadd.sh
+++ cvsadd.sh
@@ -1,1 +1,2 @@
-cvs up | sed 's/\? //g' | grep -v "^M " | grep -v "^tmp" | grep -v "^log" | grep -v "^public"
+# Il faut ajouter un "| xargs cvs add", apres voir vérifié que ça passe
+cvs up | sed 's/\? //g' | grep -v "^M " | grep -v "^tmp" | grep -v "^log" | grep -v "^public" | grep -v "^A "
