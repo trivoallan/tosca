e456bcf1adc2a7d3aec16667119352cee170b385
mloiseleur <mloiseleur>
2006-12-12 09:00:07 +0000

    *** empty log message ***

--- app/models/competence.rb
+++ app/models/competence.rb
@@ -2,4 +2,8 @@
 # Copyright Linagora SA 2006 - Tous droits réservés.#
 #####################################################
 class Competence < ActiveRecord::Base
+
+  def to_s
+    nom
+  end
 end
