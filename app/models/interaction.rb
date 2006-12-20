2fe1dd8758175faeb46e960f758e82f764d1d8d6
gdufloux <gdufloux>
2006-12-20 16:26:02 +0000

    conflict solved

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -2,13 +2,14 @@
 # Copyright Linagora SA 2006 - Tous droits réservés.#
 #####################################################
 class Interaction < ActiveRecord::Base
+
   belongs_to :logiciel, :counter_cache => true
   belongs_to :ingenieur, :counter_cache => true, :include => [:identifiant]
   has_one :reversement
 
-  def self.content_columns
+  def self.content_columns 
     @content_columns ||= columns.reject { |c| c.primary || 
-        c.name =~ /(_id|_on)$/ || c.name == inheritance_column }     
+        c.name =~ /(_id|url_de_suivi)$/ || c.name == inheritance_column } 
   end
 
 end
