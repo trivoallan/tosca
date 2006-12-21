a5c40e36d7852f83ae238f710fc132c351628cb0
mloiseleur <mloiseleur>
2006-12-21 13:43:09 +0000

    fix migration

--- db/migrate/029_alter_support.rb
+++ db/migrate/029_alter_support.rb
@@ -1,5 +1,5 @@
 class AlterSupport < ActiveRecord::Migration
-  def self.u
+  def self.up
     change_column :supports, :maintenance, :boolean
     change_column :supports, :veille_technologique, :boolean
     change_column :supports, :assistance_tel, :boolean
@@ -7,7 +7,7 @@ class AlterSupport < ActiveRecord::Migration
   end
 
   def self.down
-    remove_column :supports, :newsletter, :boolean
+    remove_column :supports, :newsletter
     # Les 3 autres colonnes étaient des enum('OUI','NON')
     # Une erreur de jeunesse :)
   end
