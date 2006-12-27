4380f0b807bb97a65d907ec5140c6b36b4020279
mloiseleur <mloiseleur>
2006-12-27 15:51:22 +0000

    ajout des demandes rox à mort

---
+++ test/unit/fichierbinaire_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class FichierbinaireTest < Test::Unit::TestCase
+  fixtures :fichierbinaires
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
