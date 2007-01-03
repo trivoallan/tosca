b9a6b7dc018461bd9c409e41be96562e8cde5e0e
sbahloul <sbahloul>
2007-01-03 17:57:00 +0000

    Minor data add in typedocuments

---
+++ db/migrate/034_new_typedemande.rb
@@ -0,0 +1,13 @@
+class NewTypedemande < ActiveRecord::Migration
+  def self.up
+    Typedemande.create(:nom => 'Monitorat')
+    Typedemande.create(:nom => 'Intervention')
+    Typedemande.create(:nom => 'Etude')
+  end
+
+  def self.down
+    Typedemande.find_by_nom('Monitorat').destroy
+    Typedemande.find_by_nom('Intervention').destroy
+    Typedemande.find_by_nom('Etude').destroy
+  end
+end
