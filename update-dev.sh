f01b88231e8cad76888ac0cc986a8e89e7d06f21
mloiseleur <mloiseleur>
2007-01-11 15:08:18 +0000

    yeah

--- update-dev.sh
+++ update-dev.sh
@@ -1,8 +1,8 @@
 sudo chown -R mloiseleur:mloiseleur *
-mkdir -p public/images/reporting
 rake log:clear
 rake tmp:clear
 cvs up
+mkdir -p public/images/reporting
 sudo chown -R www-data:www-data *
 sudo /etc/init.d/apache2 restart
 
