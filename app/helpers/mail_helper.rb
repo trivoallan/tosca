50e0460413de2b717dace5f819941e3f1666d15f
gdufloux <gdufloux>
2007-01-23 15:52:59 +0000

    *** empty log message ***

--- app/helpers/mail_helper.rb
+++ app/helpers/mail_helper.rb
@@ -3,6 +3,8 @@
 #####################################################
 module MailHelper
 
+  # met en forme du texte sur [width] colonnes
+  # TODO : ne pas supprimer les saut de lignes (cf mails)
   def wrap(s, width=78)
     s.gsub!(/(.{1,#{width}})(\s+|\Z)/, "\\1\n")
   end
