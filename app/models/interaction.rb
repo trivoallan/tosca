c4be399a0938a36331021979921401e95da153d9
gdufloux <gdufloux>
2007-01-26 07:35:39 +0000

    temporaire : id_mantis et demande en attente du migrate

--- app/models/interaction.rb
+++ app/models/interaction.rb
@@ -2,8 +2,11 @@
 # Copyright Linagora SA 2006 - Tous droits réservés.#
 #####################################################
 class Interaction < ActiveRecord::Base
-  belongs_to :client, :counter_cache => true
+
   belongs_to :logiciel, :counter_cache => true
+  # TODO : remplacer relation :client par :demande
+  # has_one :demande 
+  belongs_to :client, :counter_cache => true
   belongs_to :ingenieur, :counter_cache => true, :include => [:identifiant]
   has_one :reversement
 
@@ -14,5 +17,15 @@ class Interaction < ActiveRecord::Base
     @content_columns ||= columns.reject { |c| c.primary || 
         c.name =~ /(_id|url_de_suivi)$/ || c.name == inheritance_column } 
   end
+
+  # TODO : supprimer mantis apres le migrate
+  def id_mantis
+    false #111
+  end
+
+  # TODO : supprimer demande apres le migrate
+  def demande
+    Demande.find_first
+  end 
 
 end
