b17e42b4d78b65946db2407864fb93a090464bbc
gdufloux <gdufloux>
2007-02-06 16:08:50 +0000

    add_column, et non add.column : non mais

--- db/migrate/036_alter_correctifs_and_reversements.rb
+++ db/migrate/036_alter_correctifs_and_reversements.rb
@@ -3,13 +3,13 @@ class AlterCorrectifsAndReversements < ActiveRecord::Migration
   # fusion des reversements et des correctif
   def self.up
     # on recupère la plupart des champs des reversements et des interactions
-    add.column :correctifs, :reverse_le, :datetime
-    add.column :correctifs, :description_fonctionnelle, :text, :default => "", :null => false
-    add.column :correctifs, :updated_on, :timestamp
-    add.column :correctifs, :etatreversement_id, :integer, :default => 0, :null => false
-    add.column :correctifs, :cloture_le, :datetime
-    add.column :correctifs, :logiciel_id, :integer, :default => 0, :null => false
-    add.column :correctifs, :ingenieur_id, :integer, :null => false
+    add_column :correctifs, :reverse_le, :datetime
+    add_column :correctifs, :description_fonctionnelle, :text, :default => "", :null => false
+    add_column :correctifs, :updated_on, :timestamp
+    add_column :correctifs, :etatreversement_id, :integer, :default => 0, :null => false
+    add_column :correctifs, :cloture_le, :datetime
+    add_column :correctifs, :logiciel_id, :integer, :default => 0, :null => false
+    add_column :correctifs, :ingenieur_id, :integer, :null => false
 
     # la liaison n'existe donc plus entre correctifs et interactions
     rename_table :reversements, :old_reversements
@@ -21,7 +21,7 @@ class AlterCorrectifsAndReversements < ActiveRecord::Migration
     end
 
     # on ajoute la notion de type de contribution
-    add.column :correctifs, :typecontribution_id, :integer, :default => 0, :null => false
+    add_column :correctifs, :typecontribution_id, :integer, :default => 0, :null => false
     create_table :typecontributions, :force => true do |t|
       t.column :nom, :string, :default => "", :null => false
       t.column :description, :text, :default => "", :null => false
@@ -31,20 +31,20 @@ class AlterCorrectifsAndReversements < ActiveRecord::Migration
   end
 
   def self.down
-    remove.column :correctifs, :reverse_le
-    remove.column :correctifs, :description_fonctionnelle
-    remove.column :correctifs, :updated_on
-    remove.column :correctifs, :etatreversement_id
-    remove.column :correctifs, :cloture_le
-    remove.column :correctifs, :logiciel_id
-    remove.column :correctifs, :ingenieur_id
+    remove_column :correctifs, :reverse_le
+    remove_column :correctifs, :description_fonctionnelle
+    remove_column :correctifs, :updated_on
+    remove_column :correctifs, :etatreversement_id
+    remove_column :correctifs, :cloture_le
+    remove_column :correctifs, :logiciel_id
+    remove_column :correctifs, :ingenieur_id
 
     # restauration de la table précédemment sauvegardée (cf self.up)
     rename_table :old_reversements, :reversements
 
     drop_table :urlreversements
     drop_table :typecontributions
-    remove.column :correctifs, :typecontribution_id
+    remove_column :correctifs, :typecontribution_id
   end
 
 end
