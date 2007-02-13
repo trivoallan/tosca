3503e29f4c2886a242e33bab209839047e994985
gdufloux <gdufloux>
2007-02-13 14:24:08 +0000

    filtres passes en show_table_form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -36,6 +36,22 @@ module ApplicationHelper
     out << '</tr></table>'
   end
 
+  # Collection doit contenir des objects qui ont un 'id' et un 'nom'
+  # objectcollection contient le tableau des objects déjà présents
+  # C'est la fonction to_s qui est utilisée pour le label
+  # Ex : hbtm_radio_button( @logiciel.competences, @competences, 'competence_ids') 
+  def hbtm_radio_button( objectcollection, collection, nom )
+    return '' if collection.nil?
+    out = ""
+    for donnee in collection
+      out << "<input type=\"radio\" id=\"#{donnee.id}\" "
+      out << "name=\"#{nom}[]\" value=\"#{donnee.id}\" "
+      out << 'checked="checked" ' if objectcollection and objectcollection.include? donnee
+      out << "> #{donnee} </input>"
+    end
+    out
+  end
+
   # select_onchange(@clients, @current_client, 'client')
   # options
   # :width limite la taille du texte en nb de caractères
@@ -82,6 +98,36 @@ module ApplicationHelper
     out << text_field('numero', '', 'size' => 3)
   end
 
+  # Titles doit contenir un tableau
+  # Champs doit contenir un tableau
+  # Les éléments de Titles et Champs doivent être affichable par to_s
+  # options 
+  # :title => Donne un titre au tableau
+  # :subtitle => Donne un sous titre au tableau
+  # Ex : show_table_form( { "TOTO", "TITI"}, { "TATA", "TUTU" }, :title => "Titre" )
+  def show_table_form(titles, champs, options = {})
+    return 'Error (titles)' unless titles and titles.size > 0
+    return 'Error (champs)' unless champs and champs.size > 0
+    return 'Error (size)' unless titles.size == champs.size
+    result = ''
+    style = "class='#{options[:class] if options[:class]}'"
+    result << "<table #{style}"
+    for i in 0..titles.size
+      unless champs[i].nil? and titles[i].nil?
+        result << '<tr>'
+        if champs[i].nil? or titles[i].nil?
+          value = ( champs[i] ? champs[i].to_s : titles[i].to_s )
+          result << '<td colspan= "2">' << value << '</td>'
+        else
+          result << '<td>' << titles[i].to_s << '</td>'
+          result << '<td>' << champs[i].to_s << '</td>'
+        end
+        result << '</tr>'
+      end
+    end
+    result << '</table>'
+    result << '<br/>'
+  end
 
   ### LIENS ABSOLUS ################################################################
 
@@ -291,6 +337,19 @@ module ApplicationHelper
   def show_help(help_text, options = {:symbol => '?'})
     "<a alt=\"#{help_text}\" title=\"#{help_text}\" >#{options[:symbol]}</a>"
   end
+
+  def show_title(title, options = {})
+    return unless title
+    result = ''
+    result << "<h1>#{title}</h1>"
+    if options[:subtitle]
+      result << "<h2>#{options[:subtitle]}</h2>"
+    else
+      result << "<br/>"
+    end
+    result << "<br/>"
+  end
+
 
   ### FILES ######################################################################
 
