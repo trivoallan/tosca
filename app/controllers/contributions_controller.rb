2ba3241a501747c183a4346a69cd91c32903b931
gdufloux <gdufloux>
2007-02-19 17:37:00 +0000

    *** empty log message ***

---
+++ app/controllers/contributions_controller.rb
@@ -0,0 +1,23 @@
+class ContributionsController < ApplicationController
+
+  model :correctif
+
+  def index
+    select
+    render :action => 'select'
+  end
+
+  def list
+    @logiciel = Logiciel.find(params[:id])
+    conditions = ["logiciel_id = ?", @logiciel.id]
+    #scope_filter do
+      @contribution_pages, @contributions = paginate :correctifs, :per_page => 10,
+      :order => "created_on DESC", :conditions => conditions
+    #end
+  end
+
+  def select
+    @logiciels = Correctif.find(:all).collect{|c| c.logiciel }.uniq
+  end
+
+end
