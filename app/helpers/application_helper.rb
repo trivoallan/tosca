dba87c98e6f60613c8b0dacea897a2a4011c8abf
rschermesser <rschermesser>
2007-02-19 16:26:34 +0000

    Page demandes/comment/ valide !

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -65,10 +65,11 @@ module ApplicationHelper
   # :onchange action si changement
   # :size hauteur du select
   def select_onchange(list, default, name, options = {})
-    options[:width] ||= 15
+    #options[:width] ||= 15
     options[:title] ||= ''
     options[:onchange] ||= 'this.form.submit();'
-    collected = list.collect{|e| [sum_up(e.nom, options[:width]), e.id] }.unshift(["#{options[:title]}", ''])
+    #collected = list.collect{|e| [sum_up(e.nom, options[:width]), e.id] }.unshift(["#{options[:title]}", ''])
+    collected = list.collect{|e| [e.nom, e.id] }.unshift(["#{options[:title]}", ''])
     select = options_for_select(collected, default.to_i)
     return select_tag(name, select, options)
   end
