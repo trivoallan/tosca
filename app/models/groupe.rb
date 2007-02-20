0e33ffcbd68a2b7680cc184c72652befc0fe0bd1
mloiseleur <mloiseleur>
2007-02-20 15:32:12 +0000

    yeah

--- app/models/groupe.rb
+++ app/models/groupe.rb
@@ -3,4 +3,15 @@
 #####################################################
 class Groupe < ActiveRecord::Base
   has_many :classifications
+
+
+  def self.set_scope(contrat_ids)
+    self.scoped_methods << { :find => { :conditions => 
+        [ 'paquets.contrat_id IN (?)', contrat_id ], 
+        :joins => 'INNER JOIN logiciels ON logiciels.groupe_id=groupes.id ' +
+        'INNER JOIN paquets ON paquets.logiciel_id=logiciels.id'
+
+      } }
+  end
+
 end
