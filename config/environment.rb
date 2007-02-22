d93229fb4869992b22d35c2db5aa5261bc89cd31
mloiseleur <mloiseleur>
2007-02-22 15:33:54 +0000

    premiere serie pour ledition wiki

--- config/environment.rb
+++ config/environment.rb
@@ -67,7 +67,14 @@ end
 #Optimization des vues : plus '\n'
 ActionView::Base.erb_trim_mode = '>'
 
-
+# Needed for textilize to work properly :/
+class RedCloth
+  # Patch for RedCloth.  Fixed in RedCloth r128 but _why hasn't released it yet.
+  # <a href="http://code.whytheluckystiff.net/redcloth/changeset/128">http://code.whytheluckystiff.net/redcloth/changeset/128</a>
+  def hard_break( text ) 
+    text.gsub!( /(.)\n(?!\n|\Z| *([#*=]+(\s|$)|[{|]))/, "\\1<br />" ) if hard_breaks 
+  end 
+end
 
 
 # Rédéfinit globalement en français les messages d'erreur
