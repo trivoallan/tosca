73c104f1992583f5d552a2185846ef9003434885
mloiseleur <mloiseleur>
2007-03-02 16:28:00 +0000

    nettoyage et dryification

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -2,10 +2,7 @@
 # Copyright Linagora SA 2006 - Tous droits réservés.#
 #####################################################
 class ContributionsController < ApplicationController
-
-  # je ne sais pas s'il sont tous nécessaire : 
-  # helper :reversements, :demandes, :paquets, :binaires, :logiciels
-  helper                  :demandes, :paquets, :binaires
+  helper :demandes, :paquets, :binaires
 
   before_filter :verifie, :only => [ :show, :edit, :update, :destroy ] 
 
@@ -23,32 +20,30 @@ class ContributionsController < ApplicationController
 
   def select
     _panel
-    #flash[:notice] = 'test'
-    @logiciels = Contribution.find(:all, :order => 'reverse_le').collect{|c| c.logiciel }.uniq
+    # TODO c'est lent. Il faut un DISTINCT et repenser ce finder
+    logiciels = Contribution.find(:all, :order => 'reverse_le')
+    @logiciels = logiciels.collect{ |c| c.logiciel }.uniq
     @partial_for_summary = 'panel'
   end
 
   def list
     flash[:notice]= flash[:notice]
-    return redirect_to :action => 'select' unless params[:id]
+    return redirect_to(:action => 'select') unless params[:id]
     unless params[:id] == 'all'
       @logiciel = Logiciel.find(params[:id])
       conditions = ["logiciel_id = ?", @logiciel.id]
     else
       conditions = nil
     end
-    #scope_filter do
-      @contribution_pages, @contributions = paginate :contributions, :per_page => 10,
-      :order => "created_on DESC", :conditions => conditions
-    #end
+    @contribution_pages, @contributions = paginate :contributions, 
+    :per_page => 10, :order => "created_on DESC", :conditions => conditions
   end
 
   def admin
-    # @count = Contribution.count
     conditions = nil
     _panel
-    @contribution_pages, @contributions = paginate :contributions, :per_page => 10, 
-    :order => 'updated_on DESC'
+    @contribution_pages, @contributions = paginate :contributions, 
+    :per_page => 10, :order => 'updated_on DESC'
     @partial_for_summary = 'panel'
   end
 
@@ -63,7 +58,8 @@ class ContributionsController < ApplicationController
   def create
     @contribution = Contribution.new(params[:contribution])
     if @contribution.save
-      flash[:notice] = 'La contribution suivante a bien été créee : </br><i>'+@contribution.description+'</i>'
+      flash[:notice] = 'La contribution suivante a bien été créee : ' + 
+        '</br><i>'+@contribution.description+'</i>'
       if params[:urlreversement]
         urlreversement = Urlreversement.new(params[:urlreversement])
         urlreversement.contribution = @contribution
@@ -87,10 +83,12 @@ class ContributionsController < ApplicationController
 
   def update
     @contribution = Contribution.find(params[:id])
-    # @contribution.paquets = Paquet.find(@params[:paquet_ids]) if @params[:paquet_ids]
-    @contribution.demandes = Demande.find(@params[:demande_ids]) if @params[:demande_ids]
+    if @params[:demande_ids]
+      @contribution.demandes = Demande.find(@params[:demande_ids]) 
+    end
     if @contribution.update_attributes(params[:contribution])
-      flash[:notice] = 'La contribution suivante a bien été mise à jour : </br><i>'+@contribution.description+'</i>'
+      flash[:notice] = 'La contribution suivante a bien été mise à jour ' + 
+        ': </br><i>'+@contribution.description+'</i>'
       redirect_to :action => 'list'
     else
       _form and render :action => 'edit'
@@ -103,7 +101,7 @@ class ContributionsController < ApplicationController
   end
 
   def ajax_paquets
-    render_text('') and return unless request.xml_http_request? and params[:id]
+    return redirect_to_home unless request.xml_http_request? and params[:id]
 
     logiciel = Logiciel.find(params[:id].to_i)
     @paquets = logiciel.paquets.find(:all, Paquet::OPTIONS)
@@ -117,7 +115,6 @@ class ContributionsController < ApplicationController
   end
 
 private
-
   def _form
     @logiciels = Logiciel.find_all
     @paquets = @contribution.paquets || []
