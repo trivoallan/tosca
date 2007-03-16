1186d1f39b3da8c43dfe1f8eb12bf63404aaedd3
mloiseleur <mloiseleur>
2007-03-16 10:49:59 +0000

    migration fixed

--- db/migrate/041_add_constraints.rb
+++ db/migrate/041_add_constraints.rb
@@ -10,18 +10,18 @@ class AddConstraints < ActiveRecord::Migration
     add_index :appels_demandes, :demande_id
     
 
-    add_columns :appels, :contrat_id, :integer, :null => false
+    add_column :appels, :contrat_id, :integer, :null => false
     add_index :appels, :contrat_id
 
     add_column :contrats, :astreinte, :boolean, :default => false, :null => false
-    add_column :support, :duree_intervention, :integer, :null => true
+    add_column :supports, :duree_intervention, :integer, :null => true
     
   end
 
   def self.down
     drop_table :appels_demandes
     remove_column :contrats, :astreinte
-    remove_column :support, :duree_intervention
+    remove_column :supports, :duree_intervention
     remove_column :appels, :contrat_id
   end
 end
