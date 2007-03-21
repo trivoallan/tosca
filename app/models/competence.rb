a2a337970b25a05b05a3ecf93af47138ee4f092e
gdufloux <gdufloux>
2007-03-21 14:42:45 +0000

    *** empty log message ***

--- app/models/competence.rb
+++ app/models/competence.rb
@@ -2,6 +2,7 @@
 # Copyright Linagora SA 2006 - Tous droits réservés.#
 #####################################################
 class Competence < ActiveRecord::Base
+  has_and_belongs_to_many :ingenieurs
 
   def to_s
     nom
