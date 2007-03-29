963c83a7450502c76faa637207712c937e4ac01f
mloiseleur <mloiseleur>
2007-03-29 16:30:13 +0000

    on oublie pas les versions. Et on oublie pas de s en debarrasser :)

--- db/migrate/045_add_cc_to_demandes.rb
+++ db/migrate/045_add_cc_to_demandes.rb
@@ -1,9 +1,11 @@
 class AddCcToDemandes < ActiveRecord::Migration
   def self.up
      add_column :demandes, :mail_cc, :string, :null => true
+     add_column :demande_versions, :mail_cc, :string, :null => true
   end
 
   def self.down
-     remove_column :demandes, :mail_cc
+    remove_column :demandes, :mail_cc
+    remove_column :demande_versions, :mail_cc
   end
 end
