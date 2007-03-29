16d1140e100fa104ffaa2e5870cf3329c508ee06
rschermesser <rschermesser>
2007-03-29 16:06:12 +0000

    C'est mieux avec ce nom

--- db/migrate/045_add_cc_to_demandes.rb
+++ db/migrate/045_add_cc_to_demandes.rb
@@ -1,6 +1,6 @@
 class AddCcToDemandes < ActiveRecord::Migration
   def self.up
-     add_column :demandes, :cc, :string, :null => true
+     add_column :demandes, :mail_cc, :string, :null => true
   end
 
   def self.down
