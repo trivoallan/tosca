8088130b4554f147df863823bcda7b98301737aa
mloiseleur <mloiseleur>
2007-04-06 16:13:19 +0000

    fix en dev

--- vendor/plugins/trunk/lib/file_column_helper.rb
+++ vendor/plugins/trunk/lib/file_column_helper.rb
@@ -76,9 +76,9 @@ module FileColumnHelper
     return nil unless relative_path
 
     url = ""
-    # MLO : j'ai enlevé le @ devant request. 
-    # TODO : ca casse rien en prod avec apache ?
-    url << request.relative_url_root.to_s if absolute
+    # MLO : ON N'ENLEVE PAS CET '@'. Ca casse les liens 
+    # des pieces jointes dans les emails
+    url << @request.relative_url_root.to_s if absolute
     url << "/"
     url << object.send("#{method}_options")[:base_url] << "/"
     url << relative_path
