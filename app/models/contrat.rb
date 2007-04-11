ef45a7d9b5f656d77aa10d9c2966bd9bef2cf1e5
gdufloux <gdufloux>
2007-04-11 12:38:46 +0000

    pitit bug de glardon (Wed Apr 11 14:16:44) sur contrats/list

--- app/models/contrat.rb
+++ app/models/contrat.rb
@@ -9,7 +9,7 @@ class Contrat < ActiveRecord::Base
   has_and_belongs_to_many :ingenieurs, :order => 'contrat_id'
 
   has_many :logiciels, :through => :paquets, :group => 
-    'id', :order => 'nom ASC'
+    'id', :order => 'logiciels.nom ASC'
   has_many :binaires, :through => :paquets
   has_many :appels
 
