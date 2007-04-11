8d7e8eebf26956afb1cb99d06b9e77d92a13ea05
mloiseleur <mloiseleur>
2007-04-11 15:45:59 +0000

    nouvelle version

--- lib/overrides.rb
+++ lib/overrides.rb
@@ -3,7 +3,7 @@
 #####################################################
 
 # TODO : faire class << Date
-class Date
+class << Date
   MONTHS = { 'Janvier' => 1, 'Février' => 2, 'Mars' => 3, 'Avril' => 4, 'Mai' => 5, 'Juin' => 6, 'Juillet' => 7, 'Août' => 8, 'Septembre'=> 9, 'Octobre' =>10, 'Novembre' =>11, 'Décembre' =>12 }
   DAYS = { 'Lundi' => 0, 'Mardi' => 1, 'Mercredi' => 2, 'Jeudi'=> 3, 'Vendredi' => 4, 'Samedi' => 5, 'Dimanche' => 6 }
   ABBR_MONTHS = { 'Jan' => 1, 'Fév' => 2, 'Mar' => 3, 'Avr' => 4, 'Mai' => 5, 'Juin' => 6, 'Juil' => 7, 'Aoû' => 8, 'Sep' => 9, 'Oct' =>10, 'Nov' =>11, 'Déc' =>12 }
