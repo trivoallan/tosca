d6b9d8eac0cdd8d9fd88421cdccbf8887ddb9067
mloiseleur <mloiseleur>
2007-06-04 15:58:55 +0000

    nouveaute dans le process. De suspendue a annuee. On pose la question au client, il repond et on annule

--- app/models/statut.rb
+++ app/models/statut.rb
@@ -21,7 +21,7 @@ class Statut < ActiveRecord::Base
     search = case id
              when 1 then 'id IN (2)'
              when 2 then 'id IN (4,3,8)'
-             when 3 then 'id IN (2,5,6,7)'
+             when 3 then 'id IN (2,5,6,7,8)'
              when 4 then 'id IN (3)'
              when 5 then 'id IN (3)'
              when 6 then 'id IN (7)'
