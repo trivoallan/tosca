5da2aa72671a4c72448f0afa9a967141f3ba216a
mloiseleur <mloiseleur>
2007-06-22 10:14:29 +0000

    gros nettoyage : plus de dl/dt

--- app/helpers/bienvenue_helper.rb
+++ app/helpers/bienvenue_helper.rb
@@ -7,26 +7,6 @@ module BienvenueHelper
     s.gsub!(/(.{1,#{width}})(\s+|\Z)/, "\\1<br />")
   end
 
-
-  # Call it like :
-  #   <%= show_liste_accueil(links, 'Groupe') %>
-  def show_liste_accueil(elements, titre, options = {})
-    elements.compact!
-    size = elements.size
-    return "" unless size > 0
-    result = ''
-    result << "<dt>#{titre.humanize}</dt>"
-    result << ' <dd class="action">'
-    # résumé des demandes
-    result << show_table_demandes(@demandes, []) { |demande|
-      "<td>#{link_to_demande demande, :show_id => true, :icon_severite => true}</td>"
-    } if options[:demandes]
-    result << '   <ul>'
-    elements.each { |e| result << "<li>#{e}</li>" }
-    result << '   </ul>'
-    result << ' </dd>'
-  end
-
   # Call it like :
   #   <% titres = ['Fichier', 'Taille', 'Auteur', 'Maj'] %>
   #   <%= show_table(@documents, Document, titres) {|e| "<td>#{e.nom}</td>"}%>
@@ -38,10 +18,10 @@ module BienvenueHelper
       titres.each {|t| result << "<th>#{t}</th>" }
       result << '</tr>'
     end
-    elements.each_index { |i|
-      result << "<tr class=\"demande_#{elements[i].statut_id}\" " +
-                " onclick=\"window.location.href='../demandes/comment/#{elements[i].id}';\" >"
-      result << yield(elements[i])
+    elements.each { |d|
+      result << "<tr class=\"demande_#{d.statut_id}\" " +
+        " onclick=\"window.location.href='../demandes/comment/#{d.id}';\" >"
+      result << yield(d)
       result << '</tr>'
     }
     result << '</table>'
