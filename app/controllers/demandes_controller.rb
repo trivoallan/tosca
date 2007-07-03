f95f7a45d0ffbed8146330f8959a96ae72db911d
agrellier <agrellier>
2007-07-03 14:22:06 +0000

    recherche uniquement dans les noms et descriptions des logiciels dans le panneau de gauche

--- app/controllers/demandes_controller.rb
+++ app/controllers/demandes_controller.rb
@@ -18,17 +18,16 @@ class DemandesController < ApplicationController
 #                               :theme_advanced_buttons2 => [],
 #                               :theme_advanced_buttons3 => []}
   def auto_complete_for_logiciel_nom
+    unless params[:logiciel][:nom]
+      redirect_to :action => 'list'
+    end
     options = '%' + params[:logiciel][:nom].downcase + '%'
 
     @logiciels = Logiciel.find(:all, :select => 'nom',
       :conditions => [ 'LOWER(nom) LIKE :recherche OR 
         LOWER(description) LIKE :recherche',{:recherche => options } ], 
       :order => 'nom ASC')
-    @resume = Logiciel.find(:all, :include => 'demandes',
-      :select => 'nom',
-      :conditions => ['demandes.resume LIKE ?', options ] )
 
-    @logiciels += [@resume].flatten
     render :partial => 'dem_auto_complete'
   end
   def index
