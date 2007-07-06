b8a1e6aa408f18b66ca2d038bf9ffc7c8cf6597c
mloiseleur <mloiseleur>
2007-07-06 07:38:18 +0000

    fonction inutilisee

--- app/helpers/bienvenue_helper.rb
+++ app/helpers/bienvenue_helper.rb
@@ -7,25 +7,5 @@ module BienvenueHelper
     s.gsub!(/(.{1,#{width}})(\s+|\Z)/, "\\1<br />")
   end
 
-  # Call it like :
-  #   <% titres = ['Fichier', 'Taille', 'Auteur', 'Maj'] %>
-  #   <%= show_table(@documents, Document, titres) {|e| "<td>#{e.nom}</td>"}%>
-  def show_table_demandes(elements, titres)
-    return "<br/><p>Aucune demande</p>" unless elements and elements.size > 0
-    result = '<table>'
-    if titres.size > 0
-      result << '<tr>'
-      titres.each {|t| result << "<th>#{t}</th>" }
-      result << '</tr>'
-    end
-    elements.each { |d|
-      result << "<tr class=\"demande_#{d.statut_id}\" " +
-        " onclick=\"window.location.href='../demandes/comment/#{d.id}';\" >"
-      result << yield(d)
-      result << '</tr>'
-    }
-    result << '</table>'
-  end
-
 end
 
