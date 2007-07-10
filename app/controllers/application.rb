e06fdc67f5c9b8814b2d04368e290d19fad0c38b
omunteanu <omunteanu>
2007-07-10 09:15:57 +0000

    fin bienvenue

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -24,7 +24,7 @@ class ApplicationController < ActionController::Base
   # périmètre limité pour certains profils
   around_filter :scope
 
-  # systems d'authentification 
+  # systems d'authentification
   include LoginSystem
   include ACLSystem
   # système de construction des filters
@@ -37,7 +37,7 @@ class ApplicationController < ActionController::Base
   # Options pour tiny_mce
   # http://wiki.moxiecode.com/index.php/TinyMCE:Configuration
   # L'option pour ne pas avoir de tinyMCE est la class "mceNoEditor".
-  # TODO : mettre le bouton "image" et le plugin "advimage", quand on aura 
+  # TODO : mettre le bouton "image" et le plugin "advimage", quand on aura
   # fini la vue sur l'upload
   uses_tiny_mce :options => { :mode => 'textareas',
                               :entity_encoding => 'raw',
@@ -52,23 +52,23 @@ class ApplicationController < ActionController::Base
                               :language => 'fr',
                               :editor_deselector => 'mceNoEditor' }
 
-protected  
+protected
   # redirection à l'accueil
   # TODO : certain redirect_to_home devrait etre redirect_back
   # TODO : faire une route nommée, c'est pas railsien cette fonction
-  # TODO : trouver une meilleure solution, comme surcharger (un peu) 
+  # TODO : trouver une meilleure solution, comme surcharger (un peu)
   # le redirect_to de l'ActionController::Base
   def redirect_to_home
     if request.xhr?
       render_text('<div class="information error">' + ERROR_MESSAGE + '</div>')
     else
-      redirect_to :controller => 'bienvenue', :action => ''
+      redirect_to :controller => 'bienvenue', :action => 'index'
     end
   end
- 
+
   # redirection par défaut en cas d'erreur / de non droit
   def redirect_back
-    redirect_back_or_default :controller => 'bienvenue', :action => ''
+    redirect_back_or_default :controller => 'bienvenue', :action => 'index'
   end
 
   # variables globales (beurk, mais tellement pratique ;))
@@ -77,7 +77,7 @@ protected
   def set_global_shortcuts
     # not needed since we use rgettext
     # headers['Content-Type'] =
-    # ( request.xhr? ? 'text/javascript; charset=utf-8' : 
+    # ( request.xhr? ? 'text/javascript; charset=utf-8' :
     # 'text/html; charset=utf-8' )
     @ingenieur = session[:ingenieur]
     @beneficiaire = session[:beneficiaire]
@@ -87,23 +87,23 @@ protected
   def self.auto_complete_for(object, method, options = {})
     define_method("auto_complete_for_#{object}_#{method}") do
       column = object.to_s.pluralize + '.' + method.to_s
-      find_options = { 
-        :conditions => [ "LOWER(#{column}) LIKE ?", 
-                         '%' + params[object][method].downcase + '%' ], 
+      find_options = {
+        :conditions => [ "LOWER(#{column}) LIKE ?",
+                         '%' + params[object][method].downcase + '%' ],
         :order => "#{column} ASC",
         :limit => 10 }.merge!(options)
-      
+
       @items = object.to_s.camelize.constantize.find(:all, find_options)
-      
+
       render :inline => "<%= auto_complete_result @items, '#{method}' %>"
     end
   end
 
 private
-  # scope imposé sur toutes les vues, 
+  # scope imposé sur toutes les vues,
   # pour limiter ce que peuvent voir nos clients
   SCOPE_CLIENT = [ Client, Demande, Document, Socle ]
-  SCOPE_CONTRAT = [ Appel, Binaire, Contrat, Paquet ] 
+  SCOPE_CONTRAT = [ Appel, Binaire, Contrat, Paquet ]
   # Cette fonction intègre un scope "maison", beaucoup plus rapide.
   # Il reste néanmoins intégralement safe
   # Le but est d'éviter les 15 imbrications de yield, trop couteuses
@@ -113,10 +113,10 @@ private
     client_ids, contrat_ids = nil, nil
     if beneficiaire
       client_ids = [ beneficiaire.client_id ]
-      contrat_ids = beneficiaire.contrat_ids 
+      contrat_ids = beneficiaire.contrat_ids
     end
     if ingenieur and not ingenieur.expert_ossa
-      contrat_ids = ingenieur.contrat_ids 
+      contrat_ids = ingenieur.contrat_ids
       client_ids = ingenieur.client_ids
     end
     SCOPE_CONTRAT.each {|m| m.set_scope(contrat_ids) } if contrat_ids
@@ -129,12 +129,12 @@ private
     end
   end
 
-  ERROR_MESSAGE = 'Une erreur est survenue. Notre service a été prévenu' + 
+  ERROR_MESSAGE = 'Une erreur est survenue. Notre service a été prévenu' +
     ' et dispose des informations nécessaires pour corriger.<br />' +
-    'N\'hésitez pas à nous contacter si le problème persiste.' 
-  WARN_NOID = 'Veuillez préciser une adresse existante et valide. Nous ne ' + 
-    'considérons pas que c\'est une erreur. Si vous pensez le contraire, ' + 
-    'n\'hésitez pas à nous contacter.' 
+    'N\'hésitez pas à nous contacter si le problème persiste.'
+  WARN_NOID = 'Veuillez préciser une adresse existante et valide. Nous ne ' +
+    'considérons pas que c\'est une erreur. Si vous pensez le contraire, ' +
+    'n\'hésitez pas à nous contacter.'
   def rescue_action_in_public(exception)
     if exception.is_a? ActiveRecord::RecordNotFound
       msg = WARN_NOID
@@ -150,20 +150,20 @@ private
       render_text('<div class="information error">' + msg + '</div>')
     else
       flash[:warn] = msg
-      redirect_to :action => '', :controller => 'bienvenue'
+      redirect_to :action => 'index', :controller => 'bienvenue'
     end
 
   end
-  
+
 
   # met le scope client en session
-  # ca permet de ne pas recharger les ids contrats 
+  # ca permet de ne pas recharger les ids contrats
   # à chaque fois
   # call it like this : scope_client(params['filters']['client_id'])
   # TODO : virer cette horreur
   def scope_client(value)
-    if value == '' 
-      nil 
+    if value == ''
+      nil
     else
       conditions = { :client_id => value.to_i }
       options = { :select => 'id', :conditions => conditions }
