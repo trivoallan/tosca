dc20cd52542b55142157b386f390d2ec0fbabe3a
omunteanu <omunteanu>
2007-07-13 16:31:45 +0000

    yjht

--- config/routes.rb
+++ config/routes.rb
@@ -98,6 +98,6 @@ ActionController::Routing::Routes.draw do |map|
   # map.connect ':controller/service.wsdl', :action => 'wsdl'
 
   # Install the default route as the lowest priority.
-  # map.connect ':controller/:action/:id'
+  map.connect ':controller/:action/:id'
 
 end
