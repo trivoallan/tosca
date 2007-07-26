ac31e5f052c072a2093ab4279b098b4d83948b20
agrellier <agrellier>
2007-07-26 14:59:25 +0000

    amélioration des tests pour le modèle client

--- test/unit/client_test.rb
+++ test/unit/client_test.rb
@@ -4,9 +4,9 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ClientTest < Test::Unit::TestCase
-  fixtures :clients
+  fixtures :clients, :typedemandes, :contrats, :beneficiaires
 
-  def test_client_create_update_delete
+  def test_client_create
     c= Client.new
     assert c.save
 
@@ -19,10 +19,6 @@ class ClientTest < Test::Unit::TestCase
     assert_not_nil c.id
     #id must be unique :
     c.update_attribute :id, 1
-    c2 = Client.new
-    assert_raise( ActiveRecord::StatementInvalid ){
-      c2.update_attribute :id, 1
-    }
 
     # Some attributes shouldn't be null :
     assert_not_nil c.nom
@@ -31,47 +27,73 @@ class ClientTest < Test::Unit::TestCase
     assert_not_nil c.adresse
     assert_not_nil c.code_acces
 
-
-    c.update_attributes(:nom => "toto", :support_id => 1, 
-      :description => "éééà @ bonj\"our", :mailingliste=> "totoaposte.net",
-      :adresse => "les champs Élysées 95000 Paris", :image_id => 1, 
-      :code_acces => "lenomdemonchien", :beneficiaires_count => 21
-      )
-    assert_equal c.nom, "toto"
-    assert_equal c.support_id, 1
-    assert_equal c.description, "éééà @ bonj\"our"
-    assert_equal c.mailingliste, "totoaposte.net"
-    assert_equal c.adresse, "les champs Élysées 95000 Paris"
-    assert_equal c.image_id, 1
-    assert_equal c.code_acces, "lenomdemonchien"
-    assert_equal c.beneficiaires_count, 21
-
-
-    #severites
-    assert_equal c.severites.length, Severite.count
-    # name :
-    assert_equal c.nom, c.to_s
-    assert_equal c.to_param, "1-toto"
-
-    #contrat_ids
-    assert_equal c.contrat_ids, '0'
-    #beneficiaire_ids
-    assert_kind_of Array, c.beneficiaire_ids
-
-    assert_equal c.logiciels, []
-    assert_equal c.ingenieurs, []
-    assert_equal c.contributions, []
-    assert_kind_of Array, c.typedemandes
-
-
-    assert c.destroy
   end
-  # Test if we can create a user, whith an id which is still used
+  # Id MUST be unique.
+  # Test if we can create a user with an id which is still used
   def test_same_id
-    assert_raise( ActiveRecord::StatementInvalid) {
-      Client.new(:id => 1).save
+    assert_raise( ActiveRecord::RecordNotSaved ) {
+      Client.new(:id => 1).save!
     }
   end
+
+  def test_update
+    c = clients( :toto )
+    c.update_attributes(:nom => "titi", :support_id => 1, 
+      :description => "la femme de toto", :mailingliste=> "titi@laposte.net",
+      :adresse => "les champs Élysées 95000 Paris", :image_id => 1, 
+      :code_acces => "lenomdeleurchien", :beneficiaires_count => 2
+      )
+    assert_equal c.nom, "titi"
+    assert_equal c.support_id, 1
+    assert_equal c.description, "la femme de toto"
+    assert_equal c.mailingliste, "titi@laposte.net"
+    assert_equal c.adresse, "les champs Élysées 95000 Paris"
+    assert_equal c.image_id, 1
+    assert_equal c.code_acces, "lenomdeleurchien"
+    assert_equal c.beneficiaires_count, 2
+  end
+  def test_destroy
+    assert clients( :toto).destroy
+  end
+  
+  def test_name
+    c = clients( :toto)
+    assert_equal c.nom, c.to_s
+    assert_equal c.to_param, "1-toto"
+  end
+  def test_severites
+    assert_equal clients(:toto).severites, Severite.find(:all)
+  end
+
+  def test_contrat_ids
+    assert_equal clients(:toto).contrat_ids, "1,2"
+    assert_equal clients(:linagorien).contrat_ids, "0"
+  end
+  def test_support_distribution
+    assert_equal clients(:toto).support_distribution, true
+    assert_equal clients(:guy).support_distribution, false
+  end
+  
+  def test_beneficiaire_ids
+    assert_equal clients(:toto).beneficiaire_ids, [1]
+    assert_equal clients(:linagorien).beneficiaire_ids, []
+  end
+  
+  def test_logiciels
+    # case customer is Linagora
+    assert_equal Logiciel.find(:all), clients(:linagorien).logiciels
+    # TODO case customer isn't Linagora
+  end
+  
+  # Need at least one request to test these tonctions
+  # TODO when request table will be migrated : improve these tests
+  def test_typedemandes
+    assert clients(:toto).typedemandes
+  end
+  def test_contributions
+    assert_equal clients(:toto).contributions, []
+  end
   
   
+
 end
