87e5de34da823d1109464d88b618cf3f69a0abe1
agrellier <agrellier>
2007-07-30 12:37:14 +0000

    Test de arch.

--- test/unit/arch_test.rb
+++ test/unit/arch_test.rb
@@ -7,7 +7,10 @@ class ArchTest < Test::Unit::TestCase
   fixtures :arches
 
   # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_to_s
+    arch = Arch.find 1
+    arch6 = Arch.find 6
+    assert_equal arch.to_s, 'i386'
+    assert_equal arch6.to_s, '<b>src</b>'
   end
 end
