2263e69750f322632bf0e2990291567be53f8ed1
agrellier <agrellier>
2007-07-30 14:05:53 +0000

    test de contrat. Certains tests ne sont pas complet; il faudra les faire après la migration des demandes/commentaires

--- test/unit/contrat_test.rb
+++ test/unit/contrat_test.rb
@@ -4,10 +4,37 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ContratTest < Test::Unit::TestCase
-  fixtures :contrats
+  fixtures :contrats, :clients
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_ouverture_formatted
+    c = Contrat.find 1
+    c_empty = Contrat.find 4
+    assert_equal c.ouverture_formatted, '26.10.2005'
+    assert_equal c_empty.ouverture_formatted, '00.00.0000'
+  end
+  def test_cloture_formatted
+    c = Contrat.find 1
+    c_empty = Contrat.find 4
+    assert_equal c.cloture_formatted, '27.10.2008'
+    assert_equal c_empty.cloture_formatted, '00.00.0000'
+  end
+  def test_find_engagement
+    #TODO when demande/commentaire will be refactored
+  end
+  def test_demandes
+    #TODO idem
+  end
+  def test_typedemandes
+    c = Contrat.find 1
+    assert_equal c.typedemandes,[]
+  end
+  
+  def test_to_s 
+    c = Contrat.find 1
+    c_empty = Contrat.find 4
+    c_name_empty = Contrat.find 5
+    assert_equal c.to_s, '1 - toto'
+    assert_equal c_empty.to_s, '4 - unknown client'
+    assert_equal c_name_empty.to_s, '5 - nil'
   end
 end
