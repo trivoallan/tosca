758576876e865d3a40d3b4e45768ed22dc3fcf92
agrellier <agrellier>
2007-07-30 13:20:52 +0000

    test des changelogs

--- test/unit/changelog_test.rb
+++ test/unit/changelog_test.rb
@@ -6,8 +6,8 @@ require File.dirname(__FILE__) + '/../test_helper'
 class ChangelogTest < Test::Unit::TestCase
   fixtures :changelogs
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_to_s
+    changelog = Changelog.find 1
+    assert_equal changelog.to_s, "11.05.2005  : Thibaut LAURENT <thibaut.laurent@oxalya.com> 6.4-1.dgi\\n- Upgrade to 6.4"
   end
 end
