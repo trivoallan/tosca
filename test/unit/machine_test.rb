aee79eeb20abf247f9f22eaed72d98b4e2545bd4
agrellier <agrellier>
2007-07-31 10:01:40 +0000

    traduction d'un message d'erreur. test des machines

--- test/unit/machine_test.rb
+++ test/unit/machine_test.rb
@@ -6,8 +6,10 @@ require File.dirname(__FILE__) + '/../test_helper'
 class MachineTest < Test::Unit::TestCase
   fixtures :machines
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_to_s
+    m = Machine.find 1
+    m_without_acces = Machine.find 2
+    assert_equal m.to_s, 'socle2004.ossa (192.168.1.106)'
+    assert_equal m_without_acces.to_s, '-'
   end
 end
