99a3590f786d5957bc87d8efba98740fc59a70cc
agrellier <agrellier>
2007-07-31 13:56:33 +0000

    les tests touchent à leurs fin ... du moins il reste les demandes :)

--- test/unit/urlreversement_test.rb
+++ test/unit/urlreversement_test.rb
@@ -6,8 +6,16 @@ require File.dirname(__FILE__) + '/../test_helper'
 class UrlreversementTest < Test::Unit::TestCase
   fixtures :urlreversements
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_presence_of_valeur
+    assert !Urlreversement.new.save
   end
+  def test_nom
+    assert_equal Urlreversement.find(1).nom, 
+      "http://openoffice.org/monbugestici.html"
+  end
+  def test_to_s
+    assert_equal Urlreversement.find(1).to_s, 
+      "http://openoffice.org/monbugestici.html"
+  end
+
 end
