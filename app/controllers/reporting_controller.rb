378eda438b0029f2bc9351b50abcf4572672bbb1
agrellier <agrellier>
2007-08-31 15:06:47 +0000

    bug dans le reporting flux sur l'action new.
    Un reporting/new redirige vers demandes/new maintenant

--- app/controllers/reporting_controller.rb
+++ app/controllers/reporting_controller.rb
@@ -31,6 +31,9 @@ class ReportingController < ApplicationController
     @contrats = (@beneficiaire ? @beneficiaire.client.contrats :
                  Contrat.find(:all, Contrat::OPTIONS))
   end
+  def new
+    redirect_to :action => 'index', :controller => :demandes
+  end
 
   def comex
     #_titles()
