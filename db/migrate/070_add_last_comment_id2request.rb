864d5d5525a463c1626bc319b657f533f5242314
mloiseleur <mloiseleur>
2007-10-03 16:10:35 +0000

    c'est mieux comme ca, non ?

--- db/migrate/070_add_last_comment_id2request.rb
+++ db/migrate/070_add_last_comment_id2request.rb
@@ -1,6 +1,10 @@
 class AddLastCommentId2request < ActiveRecord::Migration
   def self.up
     add_column :demandes, :last_comment_id, :integer, :default => 0, :null => false
+    update("UPDATE demandes d " + 
+           "SET last_comment_id=(SELECT c.id FROM commentaires c " + 
+           "WHERE c.demande_id=d.id ORDER BY c.created_on DESC LIMIT 1)" + 
+           "WHERE d.last_comment_id=0" )
   end
 
   def self.down
