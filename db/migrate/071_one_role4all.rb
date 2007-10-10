885a677bb2932a2cfbcbb0b600b0a5e56d68690e
mloiseleur <mloiseleur>
2007-10-10 15:14:28 +0000

    oups

---
+++ db/migrate/071_one_role4all.rb
@@ -0,0 +1,17 @@
+class OneRole4all < ActiveRecord::Migration
+  def self.up
+    add_column :identifiants, :role_id, :integer, :null => false, :default => 0
+    update("UPDATE identifiants i SET i.role_id = " <<
+           "(SELECT ir.role_id FROM identifiants_roles ir INNER JOIN roles r" <<
+           " ON r.id=ir.role_id WHERE ir.identifiant_id=i.id)")
+    # inactive clients were made with no role, initially.
+    update("UPDATE identifiants i SET i.role_id = 2, r.inactive = 1 " << 
+           "WHERE i.role_id IS NULL")
+    drop_table :identifiants_roles
+  end
+
+  def self.down
+    remove_column :identifiants, :role_id
+  end
+
+end
