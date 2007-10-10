7872e0c60cb62f3d0bd27fda7c9746dbbd5f5368
rschermesser <rschermesser>
2007-10-10 15:20:27 +0000

    Mise à jour du script de migration

--- db/migrate/071_one_role4all.rb
+++ db/migrate/071_one_role4all.rb
@@ -4,8 +4,8 @@ class OneRole4all < ActiveRecord::Migration
     update("UPDATE identifiants i SET i.role_id = " <<
            "(SELECT ir.role_id FROM identifiants_roles ir INNER JOIN roles r" <<
            " ON r.id=ir.role_id WHERE ir.identifiant_id=i.id)")
-    # inactive clients were made with no role, initially.
-    update("UPDATE identifiants i SET i.role_id = 2, r.inactive = 1 " << 
+#     inactive clients were made with no role, initially.
+    update("UPDATE identifiants i SET i.role_id = 2, i.inactive = 1 " <<
            "WHERE i.role_id IS NULL")
     drop_table :identifiants_roles
   end
