38ea2311798cab5a48fd5bbe052517c4d59d3acb
mloiseleur <mloiseleur>
2007-10-31 06:52:55 +0000

    finition

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,11 +16,13 @@ RAILS_DEFAULT_LOGGER = Hodel3000CompliantLogger.new(config.log_path)
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 
+config.log_level = :error
+
 # require 'hodel_3000_compliant_logger'
 # config.logger = Hodel3000CompliantLogger.new(config.log_path)
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
-# config.action_controller.asset_host                  = "http://assets.example.com"
+# config.action_controller.asset_host = "http://assets.example.com"
 
 # Disable delivery errors if you bad email addresses should just be ignored
 config.action_mailer.raise_delivery_errors = false
