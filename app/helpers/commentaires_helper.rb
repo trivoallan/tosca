631c45bdfaf60e5677b3e098f2edb04cac232cbf
mloiseleur <mloiseleur>
2007-11-13 14:21:45 +0000

    on affiche le nom de l'auteur des modifs

--- app/helpers/commentaires_helper.rb
+++ app/helpers/commentaires_helper.rb
@@ -5,20 +5,26 @@ module CommentairesHelper
 
   def display_history(comment)
     out = []
+    nom = "<b>#{comment.identifiant.nom}</b>"
+
     ingenieur = comment.ingenieur
     unless ingenieur.nil?
-      out << (_('The <b>owner</b> of your request is') + 
-              " <b>#{ingenieur.nom}</b>")
+      if ingenieur.identifiant_id == comment.identifiant_id
+        out << (_('This request has been taken into account by %s') % nom )
+      else
+        out << (_('This request has been assigned to %s by %s') % 
+                [ "<b>#{ingenieur.nom}</b>", nom ])
+      end
     end 
     statut = comment.statut
     unless statut.nil?
-      out << (_('The <b>status</b> of your request is') + 
-              " <b>#{statut.nom}</b>")
+      out << (_('This request has been changed in %s by %s') % 
+              [ "<b>#{statut.nom}</b>", nom ])
     end 
     severite = comment.severite
     unless severite.nil?
-      out << (_('The <b>severity</b> of your request is') +
-              " <b>#{severite.nom}</b>")
+      out << (_('This request has been requalified in %s by %s') % 
+              [ "<b>#{severite.nom}</b>" , nom ])
     end
     return nil if out.empty?
     '<div class="history">' << out.join('<br />') << '</div>'
