6e47c8c683bed6b69a6628780bc6f471b42a6ac5
mloiseleur <mloiseleur>
2007-11-13 14:31:29 +0000

    trad

--- app/helpers/commentaires_helper.rb
+++ app/helpers/commentaires_helper.rb
@@ -10,20 +10,20 @@ module CommentairesHelper
     ingenieur = comment.ingenieur
     unless ingenieur.nil?
       if ingenieur.identifiant_id == comment.identifiant_id
-        out << (_('This request has been taken into account by %s') % nom )
+        out << (_('This request has been taken into account by %s.') % nom )
       else
-        out << (_('This request has been assigned to %s by %s') % 
+        out << (_('This request has been assigned to %s by %s.') % 
                 [ "<b>#{ingenieur.nom}</b>", nom ])
       end
     end 
     statut = comment.statut
     unless statut.nil?
-      out << (_('This request has been changed in %s by %s') % 
+      out << (_('This request has been changed in %s by %s.') % 
               [ "<b>#{statut.nom}</b>", nom ])
     end 
     severite = comment.severite
     unless severite.nil?
-      out << (_('This request has been requalified in %s by %s') % 
+      out << (_('This request has been requalified in %s by %s.') % 
               [ "<b>#{severite.nom}</b>" , nom ])
     end
     return nil if out.empty?
