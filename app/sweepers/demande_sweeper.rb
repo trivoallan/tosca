dc43cf86ea8e4499bc997f04bb17ed3dbd97448d
mloiseleur <mloiseleur>
2007-11-16 11:38:59 +0000

    some doc

--- app/sweepers/demande_sweeper.rb
+++ app/sweepers/demande_sweeper.rb
@@ -1,6 +1,11 @@
 class DemandeSweeper < ActionController::Caching::Sweeper
   # There's a separate sweeper for Comments attached to a request.
   # We can keep them separate, so we do :).
+  # But take notes that those 2 others fragements are used :
+  #  "#{record.id}/comments"
+  #  "#{record.demande_id}/true/requests/front"
+  #  "#{record.demande_id}/false/requests/front"
+
   observe Demande 
 
   # If our sweeper detects that a Request was created call this
