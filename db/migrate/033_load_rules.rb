33b07afaf2db044141f88db06a02bc980a197842
mloiseleur <mloiseleur>
2007-12-24 15:12:40 +0000

    on ameliore encore les migrations

---
+++ db/migrate/033_load_rules.rb
@@ -0,0 +1,15 @@
+class LoadRules < ActiveRecord::Migration
+  class Ossa < ActiveRecord::Base; end
+  class TimeTicket < ActiveRecord::Base; end
+
+  def self.up
+    Ossa.create(:name => 'Ossa Illimitée', :max => -1)
+    TimeTicket.create(:name => 'Support Gold', :max => 160)
+    TimeTicket.create(:name => 'Support Silver', :max => 80)
+  end
+
+  def self.down
+    Ossa.find(:all).each{ |o| o.destroy }
+    TimeTicket.find(:all).each{ |tt| tt.destroy }
+  end
+end
--- db/migrate/037_move_mailinglist2contract.rb
+++ db/migrate/037_move_mailinglist2contract.rb
@@ -1,9 +1,13 @@
 class MoveMailinglist2contract < ActiveRecord::Migration
-  class Contrat < ActiveRecord::Base; end
-  class Client < ActiveRecord::Base; end
+  class Contrat < ActiveRecord::Base
+    belongs_to :client
+  end
+  class Client < ActiveRecord::Base
+    has_many :contrats
+  end
 
   def self.up
-    add_column :contrats, :mailinglist, :string, :limit => 50, :null => false
+    # add_column :contrats, :mailinglist, :string, :limit => 50, :null => false
     Client.find(:all).each do |cl|
       ml = cl.mailingliste
       cl.contrats.each { |co| co.update_attribute :mailinglist, ml }
