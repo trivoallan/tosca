b81fa5469c23cd493785c2a1531a0f162cf4ade2
mloiseleur <mloiseleur>
2007-12-27 09:39:57 +0000

    une seule solution l'indexisation

--- db/migrate/038_add_submitter_id_to_request.rb
+++ db/migrate/038_add_submitter_id_to_request.rb
@@ -3,6 +3,8 @@ class AddSubmitterIdToRequest < ActiveRecord::Migration
     add_column :demandes, :submitter_id, :integer, :null => false
     update("UPDATE demandes d, beneficiaires b " <<
            "SET submitter_id=b.user_id WHERE b.id = d.beneficiaire_id")
+
+    add_index :demandes, :submitter_id
   end
 
   def self.down
