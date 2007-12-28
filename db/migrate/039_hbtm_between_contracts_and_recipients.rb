b56c72f4fe43bc79858073597837139d2710a7a4
mloiseleur <mloiseleur>
2007-12-28 13:15:45 +0000

    on voit plein de regressions quand on se connecte avec le compte viewer

--- db/migrate/039_hbtm_between_contracts_and_recipients.rb
+++ db/migrate/039_hbtm_between_contracts_and_recipients.rb
@@ -1,4 +1,15 @@
 class HbtmBetweenContractsAndRecipients < ActiveRecord::Migration
+  class Beneficiaire < ActiveRecord::Base
+    belongs_to :client
+    has_and_belongs_to_many :contrats
+  end
+  class Client < ActiveRecord::Base
+    has_many :contrats
+  end
+  class Contrat < ActiveRecord::Base
+    belongs_to :client
+  end
+
   def self.up
     create_table :beneficiaires_contrats, :id => false do |t|
       t.column :beneficiaire_id, :integer
@@ -6,6 +17,11 @@ class HbtmBetweenContractsAndRecipients < ActiveRecord::Migration
     end
     add_index :beneficiaires_contrats, :beneficiaire_id
     add_index :beneficiaires_contrats, :contrat_id
+
+    Beneficiaire.find(:all).each {|b|
+      b.contrats = b.client.contrats
+      b.save
+    }
   end
 
   def self.down
