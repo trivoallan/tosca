69fb8517c6e0f508aecdaba32803128b022a552e
mloiseleur <mloiseleur>
2008-01-21 16:44:52 +0000

    migration un chouilla plus correcte

--- db/migrate/042_create_rules_components.rb
+++ db/migrate/042_create_rules_components.rb
@@ -1,9 +1,20 @@
 class CreateRulesComponents < ActiveRecord::Migration
   def self.up
     rename_table :ossas, :components
+    Contrat.find(:all).each { |c|
+      if c.rule_type == 'Ossa'
+        c.update_attribute :rule_type, 'Rules::Component'
+      end
+    }
   end
 
   def self.down
     rename_table :components, :ossas
+    Contrat.find(:all).each { |c|
+      if c.rule_type == 'Rules::Component'
+        c.update_attribute :rule_type, 'Ossa'
+      end
+    }
+
   end
 end
--- db/migrate/043_create_rules_credits.rb
+++ db/migrate/043_create_rules_credits.rb
@@ -1,9 +1,20 @@
 class CreateRulesCredits < ActiveRecord::Migration
   def self.up
     rename_table :time_tickets, :credits
+    Contrat.find(:all).each { |c|
+      if c.rule_type == 'TimeTicket'
+        c.update_attribute :rule_type, 'Rules::Credit'
+      end
+    }
   end
 
   def self.down
     rename_table :credits, :time_tickets
+        Contrat.find(:all).each { |c|
+      if c.rule_type == 'Rules::Credit'
+        c.update_attribute :rule_type, 'TimeTicket'
+      end
+    }
+
   end
 end
