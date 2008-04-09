d7348ce678965219a0ab46736238d39f5606a056
xlamien <xlamien>
2008-04-09 09:26:13 +0000

    .

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -125,7 +125,7 @@ EOS
   end
 
   def link_to_tobd_requests
-    link_to(_('Requests to be done'), en_attente_demandes_path, :title =>
+    link_to(_('Pending Requests'), en_attente_demandes_path, :title =>
             _('Consult requests which are waiting an action from you'))
   end
 
