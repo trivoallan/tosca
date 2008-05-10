acb57a363fd728933f6142c997d12f0a571186cf
mloiseleur <mloiseleur>
2008-05-10 13:24:25 +0000

    generiser plus pour dormir plus

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -15,9 +15,8 @@ class Notifier < ActionMailer::Base
 
   # Notifie un état d'erreur
   def error_message(exception, trace, session, params, env)
-    @recipients = "mloiseleur@linagora.com"
-    #@recipients = "rschermesser@linagora.com"
-    #@cc = 'lstm-devel@08000linux.com'
+    @recipients = App::DeveloppersEmail
+    @cc = App::MaintenerEmail
     @from = FROM
     @content_type = HTML_CONTENT
     @subject = "Time to fix this one : #{env['REQUEST_URI']}"
