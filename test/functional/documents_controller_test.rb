95bbae302f814b13e6634431ea4fbb5dc143ee84
mloiseleur <mloiseleur>
2008-05-13 14:01:04 +0000

    mise en conformite des tests pour les documents. La regression constatee ce matin est desormais detectable via les tests.

--- test/functional/documents_controller_test.rb
+++ test/functional/documents_controller_test.rb
@@ -2,97 +2,90 @@
 # Copyright Linagora SA 2006 - Tous droits réservés.#
 #####################################################
 require File.dirname(__FILE__) + '/../test_helper'
-require 'documents_controller'
 
-# Re-raise errors caught by the controller.
-class DocumentsController; def rescue_action(e) raise e end; end
-
-class DocumentsControllerTest < Test::Unit::TestCase
-  fixtures :documents, :typedocuments, :clients
-
-  def setup
-    @controller = DocumentsController.new
-    @request    = ActionController::TestRequest.new
-    @response   = ActionController::TestResponse.new
-    login 'admin', 'admin'
-  end
+class DocumentsControllerTest < ActionController::TestCase
+  fixtures :documents, :typedocuments, :clients, :roles, :permissions,
+    :permissions_roles, :users
 
   def test_index
-    get :index
-    assert_response :success
-    assert_template 'select'
+    %w(admin manager expert customer viewer).each { |l|
+      login l, l
+      get :index
+      assert_response :success
+      assert_template 'select'
+      assert_not_nil assigns(:typedocuments)
+    }
   end
 
   def test_list
-    get :list, :id => 1
+    %w(admin manager expert customer viewer).each { |l|
+      login l, l
+      get :list, :id => 1
 
-    assert_response :success
-    assert_template 'list'
-
-    assert_not_nil assigns(:documents)
+      assert_response :success
+      assert_template 'list'
+      assert_not_nil assigns(:documents)
+    }
   end
 
   def test_show
-    get :show, :id => 1
+    %w(admin manager expert).each { |l|
+      login l, l
+      get :show, :id => 1
 
-    assert_response :success
-    assert_template 'show'
+      assert_response :success
+      assert_template 'show'
 
-    assert_not_nil assigns(:document)
-    assert assigns(:document).valid?
-  end
-
-  def test_new
-    get :new
-
-    assert_response :success
-    assert_template 'new'
-
-    assert_not_nil assigns(:document)
+      assert_not_nil assigns(:document)
+      assert assigns(:document).valid?
+    }
   end
 
   def test_create
-    num_documents = Document.count
+    %w(admin manager expert).each { |l|
+      login l, l
+      num_documents = Document.count
+      get :new
 
-    post :create, :document => {
-      :version => 1,
-      :created_on => "2006-09-05 18:08:49",
-      :updated_on => "2007-07-26 11:35:58",
-      :client_id => 1,
-      :description => "Etude réalisée pour SI2 dans le cadre de\r\nl'évaluation des solutions Open Source comme alternative à Lotus Notes",
-      :file => uploaded_png("#{File.expand_path(RAILS_ROOT)}/test/fixtures/upload_document.png"),
-      :title => "Etude Migration Lotus (SI2)",
-      :user_id => 1 ,
-      :date_delivery => nil,
-      :typedocument_id => 1
+      assert_response :success
+      assert_template 'new'
+      assert_not_nil assigns(:document)
+
+      form = select_form 'main_form'
+      form.document.title = "this is a legal picture"
+      form.document.file = uploaded_png("#{File.expand_path(RAILS_ROOT)}/test/fixtures/upload_document.png")
+      form.submit
+
+      assert flash.has_key?(:notice)
+      assert !flash.has_key?(:warning)
+      assert_response :redirect
+      assert_redirected_to :action => 'select'
+
+      assert_equal num_documents + 1, Document.count
     }
-
-    assert flash.has_key?(:notice)
-    assert_response :redirect
-    assert_redirected_to :action => 'select'
-
-    assert_equal num_documents + 1, Document.count
-  end
-
-  def test_edit
-    get :edit, :id => 1
-
-    assert_response :success
-    assert_template 'edit'
-
-    assert_not_nil assigns(:document)
-    assert assigns(:document).valid?
   end
 
   def test_update
-    post :update, :id => 1
-    assert_response :redirect
-    assert_redirected_to :action => 'show',
-      :id => '1-A-Many-to-Many-tutorial-for-Rails'
+    %w(admin manager expert).each { |l|
+      login l, l
+      get :edit, :id => 1
+      assert_response :success
+      assert_template 'edit'
+      assert_not_nil assigns(:document)
+
+      form = select_form 'main_form'
+      form.document.title = "this is a new title"
+      form.submit
+
+      assert_response :redirect
+      assert_redirected_to :action => 'show'
+    }
   end
 
   def test_destroy
-    assert_not_nil Document.find(1)
+    login 'admin', 'admin'
+    document = Document.find(1)
+    assert_not_nil document
 
     post :destroy, :id => 1
     assert_response :redirect
