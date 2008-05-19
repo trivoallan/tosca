a4116e7577871fe552db9f82b794d0d7e9971c00
mloiseleur <mloiseleur>
2008-05-19 05:49:45 +0000

    maj des fichiers de maj, en attendant capi

--- update-dev.sh
+++ update-dev.sh
@@ -2,14 +2,8 @@ sudo chown -R mloiseleur:mloiseleur *
 rake tmp:clear
 rake log:clear
 cvs up
-sudo rm -Rf vendor/plugins/tiny_mce_gzip/
-rake makemo
-
-#! update-db.sh
-export RAILS_ENV=production
-echo "Demande.connection.connection.query('UPDATE schema_info SET version = 1')" | ./script/console
 rake db:migrate
-./script/runner update-db.rb
 rake makemo
+./script/runner update-db.rb
 sudo chown -R www-data:www-data *
 sudo /etc/init.d/mongrel_cluster restart
--- update-prod.sh
+++ update-prod.sh
@@ -1,8 +1,8 @@
 sudo chown -R mloiseleur:niveau3 *
 rake tmp:clear
 cvs up
-rake makemo
 rake db:migrate
+rake makemo
 sudo chown -R www-data:www-data *
 sudo /etc/init.d/mongrel_cluster restart
 
