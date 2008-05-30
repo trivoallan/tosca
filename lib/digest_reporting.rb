003943515337a3088b424993eba7cbcd30f76ab4
rschermesser <rschermesser>
2008-05-30 16:27:20 +0000

    Nouveau reporting pas du tout prêt à être utilisé

--- lib/digest_reporting.rb
+++ lib/digest_reporting.rb
@@ -5,7 +5,6 @@ module DigestReporting
   #request is a Demande, request_at is a Demande, comments is an array of Commentaire
   DigestRequests = Struct.new(:request, :request_at, :comments)
 
-  # When put it for mails, we'll need to log in or, at the least, scope to contrat_ids
   def digest_result(period)
     @period = period
     @period = "year" if period.blank?
@@ -37,6 +36,50 @@ module DigestReporting
 
       last_contrat_id = r.contrat_id
     end
+  end
+
+  #important is an array of Demande, other is an array of DigestContrats
+  DigestManagers = Struct.new(:important, :other)
+
+  def digest_managers(period)
+    @period = period
+    @period = "year" if period.blank?
+    updated = Time.now.send("beginning_of_#{@period}")
+    # We must localise it after getting the (english) helper for the start date
+    @period = _(@period)
+    
+    options = { :conditions => [ "updated_on >= ? ", updated ],
+     :order => "contrat_id ASC", :include => [:typedemande, :severite, :statut]}
+    requests = Demande.find(:all, options)
+    
+    @result = DigestManagers.new    
+    @result.important = Array.new
+    @result.other = Array.new
+    last_contrat_id = nil
+    requests.each do |r|
+      if last_contrat_id != r.contrat_id and not r.critical?
+        dc = DigestContrats.new
+        dc.contract = r.contrat
+        dc.requests = Array.new
+        @result.other.push(dc)
+      end
+      
+      if r.critical?
+        @result.important.push(r)
+      else 
+ 
+        options = { :conditions => [ "created_on >= ? ", updated ] }
+
+        dr  = DigestRequests.new
+        dr.request = r
+        dr.request_at = r.state_at(updated)
+        dr.comments = r.commentaires.find(:all, options)
+        @result.other.last.requests.push(dr)
+      end
+
+      last_contrat_id = r.contrat_id
+    end
+
   end
 
 end
