6face3a7ee2f52246ff2681b53fa11c2594f8ede
mloiseleur <mloiseleur>
2008-06-02 10:19:07 +0000

    fix

--- db/migrate/049_create_team.rb
+++ db/migrate/049_create_team.rb
@@ -1,16 +1,16 @@
-class AddTeam < ActiveRecord::Migration
+class CreateTeam < ActiveRecord::Migration
   def self.up
     create_table :teams do |t|
       t.string :name, :motto
       t.integer :user_cache, :contact_id
     end
-    
+
     create_table :users_teams do |t|
       t.integer :user_id, :team_id
     end
     add_index :users_teams, :user_id
     add_index :users_teams, :team_id
-    
+
     create_table :contrats_teams do |t|
       t.integer :contrat_id, :team_id
     end
