1aafac9eb146d1823763ab4d34f8403de1ef6ffb
rschermesser <rschermesser>
2008-06-10 06:42:08 +0000

    Migration !! NetBeans is coooool

--- update-db.rb
+++ update-db.rb
@@ -7,10 +7,10 @@ p "roles cleaned"
 Demande.find(:all).each{|r| r.reset_elapsed }
 p "requests reseted"
 
-classes = [ User, Contrat, Demande ]
+classes = [ User, Contract, Demande ]
 classes.each{|c| c.record_timestamps = false }
 User.find(:all).each {|u| u.update_attribute :email, 'mloiseleur@linagora.com'}
-Contrat.find(:all).each {|c| c.update_attribute :mailinglist, 'mloiseleur@linagora.com' }
+Contract.find(:all).each {|c| c.update_attribute :mailinglist, 'mloiseleur@linagora.com' }
 Demande.find(:all).each {|d| d.update_attribute :mail_cc, nil }
 classes.each{|c| c.record_timestamps = true }
 p "emails erased"
