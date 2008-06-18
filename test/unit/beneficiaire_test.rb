b840fff6f03c4bf8b00b9fadb8197838eab9f214
Rémy-Christophe Schermesser <git@schermesser.com>
2008-06-18 18:43:34 +0200

    Some translation Some better tests

--- test/unit/beneficiaire_test.rb
+++ test/unit/beneficiaire_test.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class BeneficiaireTest < Test::Unit::TestCase
-  fixtures :beneficiaires, :clients, :contracts, :users
+  fixtures :beneficiaires, :clients, :contracts, :users, :contracts_users
 
   def test_to_strings
     check_strings Beneficiaire
@@ -11,4 +11,12 @@ class BeneficiaireTest < Test::Unit::TestCase
     Beneficiaire.find(:all).each { |b| check_ids b.contract_ids, Contract }
   end
 
+  def test_contracts
+    customer = beneficiaires(:beneficiaire_00001)
+    contracts = customer.user.contracts
+    
+    assert_equal contracts.size, 1
+    assert_equal contracts.first.id, 1
+  end
+  
 end
