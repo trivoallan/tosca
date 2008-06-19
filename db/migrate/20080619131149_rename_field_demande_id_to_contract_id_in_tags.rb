db16b1ab350839d9a65e8189b358793e4da75710
Julien HEYMAN <jheyman@linagora.com>
2008-06-19 15:20:14 +0200

    Rename field in tags to fix a bug

---
+++ db/migrate/20080619131149_rename_field_demande_id_to_contract_id_in_tags.rb
@@ -0,0 +1,9 @@
+class RenameFieldDemandeIdToContractIdInTags < ActiveRecord::Migration
+  def self.up
+    rename_column :tags, :demande_id, :contract_id
+  end
+
+  def self.down
+    rename_column :tags, :contract_id, :demande_id
+  end
+end
