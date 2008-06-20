5c87c43e8935ac67019c861e5e0c88ac43ab4936
Michel Loiseleur <mloiseleur@linagora.com>
2008-06-20 15:56:35 +0200

    0.7.3rc4

--- lib/utils.rb
+++ lib/utils.rb
@@ -8,7 +8,7 @@ module App
 
   # application
   Name = "TOSCA"
-  Version = "0.7.3rc3"
+  Version = "0.7.3rc4"
   Copyright = " ©2006-2008 Linagora SA".gsub(' ','&nbsp;')
   FilesPath = File.join RAILS_ROOT, 'files'
 
