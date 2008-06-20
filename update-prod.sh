5ea6ee4e6f9eebc56aced5246d1b25cab50b31ae
Michel Loiseleur <mloiseleur@linagora.com>
2008-06-20 11:34:12 +0200

    work on flash generated 4 festival alerts

--- update-prod.sh
+++
@@ -1,7 +0,0 @@
-sudo chown -R mloiseleur:niveau3 *
-rake tmp:clear
-cvs up
-rake db:migrate
-rake l10n:mo
-sudo chown -R www-data:www-data *
-sudo /etc/init.d/mongrel_cluster restart
--- vendor/plugins/festivaltts_on_rails/lib/festivaltts_on_rails.rb
+++ vendor/plugins/festivaltts_on_rails/lib/festivaltts_on_rails.rb
@@ -2,7 +2,7 @@
 require 'digest/sha1'
 require "#{File.dirname(__FILE__)}/festival4r.rb"
 
-MP3_FLASH_PLAYER_URL = "/flash/dewplayer-mini.swf"
+MP3_FLASH_PLAYER_URL = "dewplayer-mini.swf"
 MP3_FOLDER_URL = "/festivaltts_mp3"
 MP3_FOLDER_PATH = "#{RAILS_ROOT}/public" + MP3_FOLDER_URL
 
@@ -17,9 +17,9 @@ end
 
 # Returns needed html for playing mp3.
 def html_for_mp3_flash(filename, width = 0, height = 0)
-  "<object type=\"application/x-shockwave-flash\"\n \
-   data=\"#{MP3_FLASH_PLAYER_URL}?mp3=#{filename}&amp;autostart=1&amp;autoreplay=1\" width=\"#{width}\"\n \
+ "<object type=\"application/x-shockwave-flash\"\n \
+   data=\"#{compute_public_path(MP3_FLASH_PLAYER_URL, "flash")}?mp3=#{compute_public_path(filename, "festivaltts_mp3")}&amp;autostart=1&amp;autoreplay=1\" width=\"#{width}\"\n \
    height=\"#{height}\">\n \
-   <param name=\"movie\" value=\"#{MP3_FLASH_PLAYER_URL}?mp3=#{filename}&amp;autostart=1&amp;autoreplay=1\" />\n \
+   <param name=\"movie\" value=\"#{compute_public_path(MP3_FLASH_PLAYER_URL, "flash")}?mp3=#{compute_public_path(filename, "festivaltts_mp3")}&amp;autostart=1&amp;autoreplay=1\" />\n \
    </object>"
 end
