23aeb3b43c3c752f9c803d5d2cd10f1067499bf4
Rémy-Christophe Schermesser <git@schermesser.com>
2008-07-11 11:48:55 +0200

    Add model package to an old migration

--- db/migrate/036_improve_package_versioning.rb
+++ db/migrate/036_improve_package_versioning.rb
@@ -1,6 +1,16 @@
 class ImprovePackageVersioning < ActiveRecord::Migration
+  class Paquet < ActiveRecord::Base
+    belongs_to :logiciel
+    belongs_to :contract, :counter_cache => true
+    belongs_to :mainteneur
+
+    has_many :changelogs, :dependent => :destroy
+    has_many :binaires, :dependent => :destroy, :include => :version
+    has_and_belongs_to_many :contributions
+  end
+  
   def self.up
-    change_column :paquets, :version, :string,  :limit => 60, :default => "x",   :null => false
+    change_column :paquets, :version, :string,  :limit => 60, :default => "x",  :null => false
     change_column :paquets, :release, :string,  :limit => 60, :default => nil, :null => true
     Paquet.find(:all).each do |p|
       p.update_attribute :release, nil if p.release == ''
