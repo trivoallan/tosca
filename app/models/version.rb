3eadb844753002bae14fc90c68cf2b1c0e8a354a
Michel Loiseleur <mloiseleur@linagora.com>
2008-07-17 16:47:16 +0200

    only one is needed ;)

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,19 +1,18 @@
 class Version < ActiveRecord::Base
   has_one :logiciel, :through => :releases
-  
+
   has_many :releases, :dependent => :destroy
   has_and_belongs_to_many :contributions
-    
+
   validates_presence_of :logiciel, :version
-    
+
   def name
     @name ||= "#{logiciel.name}-#{version}"
-    @name
   end
-  
+
   def self.set_scope(contract_ids)
     self.scoped_methods << { :find => { :conditions =>
       [ 'versions.contract_id IN (?)', contract_ids ]} }
   end
-  
+
 end
