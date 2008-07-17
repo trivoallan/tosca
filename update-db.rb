b31c9b156b0bc208e29b41fe240956c9d1299a30
Michel Loiseleur <mloiseleur@linagora.com>
2008-07-17 18:33:21 +0200

    remove manage file

--- update-db.rb
+++
@@ -1,7 +0,0 @@
-classes = [ User, Contract, Demande ]
-classes.each{|c| c.record_timestamps = false }
-User.find(:all).each {|u| u.update_attribute :email, App::MaintenerEmail }
-Contract.find(:all).each {|c| c.update_attribute :mailinglist, App::MaintenerEmail }
-Demande.find(:all).each {|d| d.update_attribute :mail_cc, nil }
-classes.each{|c| c.record_timestamps = true }
-p "emails erased"
