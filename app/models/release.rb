9cc8f3732961156f754b12d577891aa1d45d43e4
Michel Loiseleur <mloiseleur@linagora.com>
2008-07-21 14:50:13 +0200

    adding scope to release

--- app/models/release.rb
+++ app/models/release.rb
@@ -2,11 +2,18 @@ class Release < ActiveRecord::Base
   belongs_to :version
   belongs_to :contract
   belongs_to :logiciel
-  
+
   has_one :changelog
-  
+
   def name
     "#{self.logiciel.name} v#{self.version.version} r#{self.release}"
   end
-  
+
+  # See ApplicationController#scope
+  def self.set_scope(contract_ids)
+    self.scoped_methods << { :find => { :conditions =>
+        [ 'relases.contract_id IN (?)', contract_ids ]
+      } } if contract_ids
+  end
+
 end
