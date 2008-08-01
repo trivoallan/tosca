db27d369428787cfdbfc6fc0cedd9c9a5139baae
Michel Loiseleur <mloiseleur@linagora.com>
2008-08-01 16:51:19 +0200

    Remove News Controller since we will use a blog system

--- app/models/new.rb
+++
@@ -1,9 +0,0 @@
-class New < ActiveRecord::Base
-  belongs_to :ingenieur
-  belongs_to :client
-  belongs_to :logiciel
-
-  def name
-    subject
-  end
-end
--- test/unit/new_test.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + '/../test_helper'
-
-class NewTest < Test::Unit::TestCase
-  fixtures :news
-
-  def test_to_strings
-    check_strings New
-  end
-end
