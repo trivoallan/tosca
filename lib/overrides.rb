ee4b2d7d5a289fc7429ee9dd4ee9e336466a98fa
Michel Loiseleur <mloiseleur@linagora.com>
2008-08-04 18:41:32 +0200

    removed last dep of ruport from the overrides

--- lib/overrides.rb
+++ lib/overrides.rb
@@ -29,23 +29,6 @@ class Module
 end
 
 
-module Ruport::Reportable::InstanceMethods
-  # Overrides, since Ruport is not really active for supporting edge rails.
-  # Maybe we can remove it with a further version of acts_as_reportable
-  # It's broken as of version 1.1.0.
-  def get_attributes_with_options(options = {})
-        attrs = attributes()
-        attrs = attrs.inject({}) {|h,(k,v)|
-                  h["#{options[:qualify_attribute_names]}.#{k}"] = v; h
-                } if options[:qualify_attribute_names]
-        attrs
-      end
-
-
-end
-
-
-
 # TODO : find a lib or a way to compute holidays
 # of other countries. It's only France, for now.
 # You can override Fixed & Variable Holidays in lib/config.rb
