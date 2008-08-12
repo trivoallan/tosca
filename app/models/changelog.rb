a9779e44d482fca350f25eea1d7176d1eb2474ed
Rémy-Christophe Schermesser <git@schermesser.com>
2008-08-12 09:03:14 +0200

    Fix error in the changelog model

--- app/models/changelog.rb
+++ app/models/changelog.rb
@@ -2,11 +2,18 @@ class Changelog < ActiveRecord::Base
   belongs_to :release
 
   def modification_date_formatted
-      display_time read_attribute(:modification_date)
+    display_time read_attribute(:modification_date)
   end
 
   def name
-    self.modification_date_formatted + ' : ' << self.name << '\n' <<
+    puts "---"
+    puts self.modification_date_formatted
+    puts "---"
+    puts read_attribute(:name)
+    puts "---"
+    puts self.modification_text
+    puts "---"
+    self.modification_date_formatted + ' : ' << read_attribute(:name) << '\n' <<
       self.modification_text
   end
 
