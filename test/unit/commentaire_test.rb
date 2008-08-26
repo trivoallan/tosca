de2aa303e496ffdda8d25248b464a8dd79682859
Rémy-Christophe Schermesser <git@schermesser.com>
2008-08-26 12:19:56 +0200

    A little new test for comments without corps

--- test/unit/commentaire_test.rb
+++ test/unit/commentaire_test.rb
@@ -44,5 +44,25 @@ class CommentaireTest < Test::Unit::TestCase
     d = Demande.find(:first)
     d.commentaires.each { d.destroy }
   end
+  
+  def test_create_commentaire_without_corps
+    #Should not work
+    Statut::NEED_COMMENT.each do |s|
+      c = Commentaire.new(:corps => "",
+        :demande => Demande.find(:first),
+        :user => User.find(:first),
+        :prive => false, :statut_id => s)
+      assert_equal(false, c.save)
+    end
+
+    #Should work
+    ([1,2,3,4,5,6,7,8] - Statut::NEED_COMMENT).each do |s|
+      c = Commentaire.new(:corps => "",
+        :demande => Demande.find(:first),
+        :user => User.find(:first),
+        :prive => false, :statut_id => s)
+      assert c.save!
+    end
+  end
 
 end
