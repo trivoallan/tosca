39ac9e0ae644cbd5c59c6caadb67daf98e3dda93
Michel Loiseleur <mloiseleur@linagora.com>
2008-10-07 15:05:10 +0200

    various display fix on issue display. csn tab is prettier & the expand/collapse usability is improved, thanks to jon's comments.

--- public/javascripts/tosca.js
+++ public/javascripts/tosca.js
@@ -14,6 +14,19 @@ function tosca_reset(dom_id) {
     }, 1);
 }
 
-function tosca_show_all_comments() {
+
+function tosca_toggle_comment(div_id) {
+	if ($(div_id).hasClassName('collapsed')) {
+		$(div_id).removeClassName('collapsed');
+	} else {
+		$(div_id).addClassName('collapsed');
+	}
+}
+
+function tosca_expand_all_comments() {
  $$('div.comment-container').invoke('removeClassName', 'collapsed');
+}
+
+function tosca_collapse_all_comments() {
+ $$('div.comment-container').invoke('addClassName', 'collapsed');
 }
