49a7f749b4a553edc8c231a6e19f3297ab869832
Michel Loiseleur <mloiseleur@linagora.com>
2008-10-14 17:06:31 +0200

    cleanup

--- test/functional/reporting_controller_test.rb
+++ test/functional/reporting_controller_test.rb
@@ -40,19 +40,4 @@ class ReportingControllerTest < ActionController::TestCase
     assert_not_nil assigns(:contracts)
   end
 
-
-=begin
-  # deactivated for now
-
-  def test_comex_cns
-    get :comex_resultat, {
-      :results => { :week_num => 33 },
-      :clients => ['all'],
-      :cns => 'Voir l\'avancement du CNS'
-    }
-    assert_response :success
-    assert_template 'comex_resultat'
-  end
-=end
-
 end
