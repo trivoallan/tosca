32c5120cb669511097c1468493dac911a784dee1
Michel Loiseleur <mloiseleur@linagora.com>
2008-12-11 15:30:51 +0100

    doc update & 80 cols

--- app/helpers/hyperlinks_helper.rb
+++ app/helpers/hyperlinks_helper.rb
@@ -1,7 +1,9 @@
 module HyperlinksHelper
+  # Call it like this :
+  #   link_to_new_hyperlink("contribution", @contribution.id)
   def link_to_new_hyperlink(model, model_id)
     return '-' if not model_id and not model
-    options = new_hyperlink_path(:model_id => model_id, :model_type => model.to_s)
-    link_to(image_create('a hyperlink'), options)
+    options = { :model_id => model_id, :model_type => model.to_s }
+    link_to(image_create('a hyperlink'), new_hyperlink_path(options))
   end
 end
