a1aa0e6576bb46417674648b6d8b6aef0834cfbe
Rémy-Christophe Schermesser <git@schermesser.com>
2008-12-12 18:48:11 +0100

    Fix bugs in the new documents views

--- db/migrate/20081212163810_update_document_model.rb
+++ db/migrate/20081212163810_update_document_model.rb
@@ -1,9 +1,11 @@
 class UpdateDocumentModel < ActiveRecord::Migration
   def self.up
     rename_column :documents, :title, :name
+    rename_column :document_versions, :title, :name
   end
 
   def self.down
     rename_column :documents, :name, :title
+    rename_column :document_versions, :name, :title
   end
 end
