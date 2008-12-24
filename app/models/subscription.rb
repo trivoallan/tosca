4e8048edc654ccca821b366fde0f62f92612de06
Michel Loiseleur <mloiseleur@linagora.com>
2008-12-24 14:36:29 +0100

    enhance active record helpers of subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,5 +1,9 @@
 class Subscription < ActiveRecord::Base
   belongs_to :user
+  # Subscription can be made on
+  # * Contract : emails for all issues
+  # * Issue : emails for a specific issue
+  # * Software : emails for all issues about a specific software
   belongs_to :model, :polymorphic => true
 
   validates_presence_of :user, :model
@@ -7,27 +11,26 @@ class Subscription < ActiveRecord::Base
     :message => _('You can be suscribe only one time on this model.')
 
   def name
-    _('Subscription for %s on %s #%s') % [ self.user.name, self.model.type, self.model.id ]
+    _('Subscription for %s on %s #%s') %
+      [ self.user.name, self.model.type, self.model.id ]
   end
 
-  def self.destroy_all(conditions = nil)
-    res = true
-    find(:all, :conditions => conditions).each do |s|
-      to_destroy = true
-      if s.model.is_a? Contract or s.model.is_a? Issue
-        subscriptions = find(:all, :conditions =>
-          { :model_type => s.model_type, :model_id => s.model_id })
-        #If there is only one subscriber to this model
-        if subscriptions.size <= 1
-          s.errors.add_to_base(_('You can not unsubscribe to this model,
-            because your are the last one watching it.'))
-          to_destroy = false
-        end
-      end
-      s.destroy if to_destroy
-      res &= to_destroy
-    end
-    res
+  def self.destroy_by_user_and_model(user, model)
+    self.first(:conditions => { :user_id => user.id, :model_type =>
+                 model.class.to_s, :model_id => model.id }).destroy
   end
-  
+
+  before_destroy :check_uniqueness
+  # For Contract, it MUST have at least one user watching it.
+  def check_uniqueness
+    return true if self.model_type != 'Contract'
+    similars = { :model_type => self.model_type, :model_id => self.model_id }
+    if self.class.count(:conditions => similars) <= 1
+      self.errors.add_to_base(_('You can not unsubscribe to this contract, because your are the last one watching it.'))
+      false
+    else
+      true
+    end
+  end
+
 end
