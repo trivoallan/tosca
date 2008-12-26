90d039c18141484d0c893dcad7c5ca7fd5d6843a
Michel Loiseleur <mloiseleur@linagora.com>
2008-12-26 09:38:38 +0100

    forgotten file

---
+++ db/migrate/20081224163311_drop_documents.rb
@@ -0,0 +1,14 @@
+class DropDocuments < ActiveRecord::Migration
+  def self.up
+    require 'fileutils'
+
+    drop_table :documents
+    drop_table :document_versions
+    drop_table :documenttypes
+    FileUtils.rm_r "#{RAILS_ROOT}/files/document"
+  end
+
+  def self.down
+    throw ActiveRecord::IrreversibleMigration
+  end
+end
