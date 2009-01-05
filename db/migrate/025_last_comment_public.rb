99aae98209282f1bdfe7ea64c72f4a3be35c69b0
Rémy-Christophe Schermesser <git@schermesser.com>
2009-01-05 11:19:07 +0100

    Update migration to work on Postgres

--- db/migrate/025_last_comment_public.rb
+++ db/migrate/025_last_comment_public.rb
@@ -17,17 +17,24 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 class LastCommentPublic < ActiveRecord::Migration
+
+  class Demande < ActiveRecord::Base; end
+  class Commentaire < ActiveRecord::Base; end
+
   def self.up
-    update("UPDATE demandes d SET d.last_comment_id = (" +
-           "SELECT id FROM commentaires c WHERE c.prive = 0 AND c.demande_id = d.id " +
-           "ORDER BY created_on DESC LIMIT 1" +
-           ")")
+    Demande.all.each do |i|
+      i.last_comment_id = Commentaire.find(:first,
+        :conditions => { :prive => false, :demande_id => i.id },
+        :order_by => 'created_on DESC').id
+      i.save!
+    end
   end
 
   def self.down
-    update("UPDATE demandes d SET last_comment_id = (" +
-           "SELECT id FROM commentaires c WHERE c.demande_id = d.id  " +
-           "ORDER BY created_on DESC LIMIT 1" +
-           ")")
+    Demande.all.each do |i|
+      i.last_comment_id = Commentaire.find(:first, :conditions => { :demande_id => i.id },
+        :order_by => 'created_on DESC').id
+      i.save!
+    end
   end
 end
--- db/migrate/038_add_submitter_id_to_request.rb
+++ db/migrate/038_add_submitter_id_to_request.rb
@@ -17,10 +17,18 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 class AddSubmitterIdToRequest < ActiveRecord::Migration
+
+  class Demande < ActiveRecord::Base; end
+
+  class Beneficiaire < ActiveRecord::Base; end
+
   def self.up
-    add_column :demandes, :submitter_id, :integer, :null => false, :default => 0
-    update("UPDATE demandes d, beneficiaires b " <<
-           "SET submitter_id=b.user_id WHERE b.id = d.beneficiaire_id")
+#    add_column :demandes, :submitter_id, :integer, :null => false, :default => 0
+
+    Demande.all.each do |d|
+      d.submitter_id = Beneficiaire.first(:conditions => { :id => d.beneficiaire_id }).user_id
+      d.save!
+    end
 
     add_index :demandes, :submitter_id
   end
