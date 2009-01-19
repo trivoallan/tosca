a451a4ba07657f755d0182eb96b3dcc560f4b14d
Rémy-Christophe Schermesser <git@schermesser.com>
2009-01-19 16:56:25 +0100

    Fix migration

--- db/migrate/20090119153646_rename_images_to_pictures.rb
+++ db/migrate/20090119153646_rename_images_to_pictures.rb
@@ -1,9 +1,15 @@
 class RenameImagesToPictures < ActiveRecord::Migration
   def self.up
-    rename_table(:images, :pictures)
+    rename_table :images, :pictures
+
+    rename_column :clients, :image_id, :picture_id
+    rename_column :users, :image_id, :picture_id
   end
 
   def self.down
-    rename_table(:pictures, :images)
+    rename_table :pictures, :images
+
+    rename_column :clients, :picture_id, :image_id
+    rename_column :users, :picture_id, :image_id
   end
 end
