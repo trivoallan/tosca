c82527f4bc3592081c057f37bda5d96ef114c4ef
Rémy-Christophe Schermesser <git@schermesser.com>
2009-01-27 23:28:13 +0100

    WIP of the new all around test

---
+++ test/functional/application_controller_test.rb
@@ -0,0 +1,50 @@
+#
+# Copyright (c) 2006-2009 Linagora
+#
+# This file is part of Tosca
+#
+# Tosca is free software, you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License, or (at your option) any later version.
+#
+# Tosca is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+require File.dirname(__FILE__) + '/../test_helper'
+
+# Each Controller Test should test all _public_ methods
+class ApplicationControllerTest < ActionController::TestCase
+
+  def test_good_display
+    routes = ActionController::Routing::Routes.routes
+    
+    #We load all the controllers
+    Dir.glob(RAILS_ROOT + '/app/controller/*.rb').each { |file| require file }
+    controllers = Object.subclasses_of(ActionController::Base).map(&:to_s)
+    
+    Permission.all.each do |p|
+      perm = Regexp.compile(p.name)
+      
+      routes.each do |r|
+        string_route = r.segments.to_s
+        if perm.match(string_route) and ((r.conditions.has_key? :method and r.conditions[:method] == :get) or r.conditions.empty?)
+          p.roles.each do |role|
+            login role.name, role.name
+            possible_controllers = controllers.grep(Regexp.compile(r.requirements[:controller], Regexp::IGNORECASE))
+            unless possible_controllers.empty?
+              @controller = eval(possible_controllers.first + ".new")
+              get r.requirements[:action], :id => 1
+              assert_response :success
+            end
+          end
+        end
+      end
+    end
+  end
+end
